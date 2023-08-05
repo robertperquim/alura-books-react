9d03ccd20028e0828025a644852905a2e8c72e12
Robert Perquim <perquimrobert@gmail.com>
2023-08-04 21:32:05 -0300

    Adicionando evento de pesquisa no projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./Componentes/Header";
 import styled from "styled-components";
+import Pesquisa from "./Componentes/Pesquisa";
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -11,6 +12,7 @@ function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
---
+++ src/Componentes/Pesquisa/DadosPesquisa.js
@@ -0,0 +1,6 @@
+export const livros = [
+  { nome: "Liderança em Design", id: 1, src: "/imagens/livro.png" },
+  { nome: "JavaScript Assertivo", id: 2, src: "/imagens/livro.png" },
+  { nome: "Cangaceiro em JavaScript", id: 3, src: "/imagens/livro.png" },
+  { nome: "Aventureiros do Java", id: 4, src: "/imagens/livro.png" },
+];
